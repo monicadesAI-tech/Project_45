d7032bdf207c8dd8f6f2fce0784defec965bef8c
monicadesAI-tech <72319489+monicadesAI-tech@users.noreply.github.com>
2020-10-16 05:43:32 +0530

    Add files via upload

---
+++ Web_Scraping_3.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# pip install beautifulsoup4
+# pip install urllib3
+
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'http://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+
+#opening up connection and grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+#html parsing
+page_soup = soup(page_html,"html.parser")
+
+#page_soup.h1
+#page_soup.p
+
+
+#grabs each product
+containers = page_soup.findAll("div",{"class":"item-container"})
+
+filename = "Products.csv"
+f = open(filename,"w")
+
+headers = "brand, product_name, shipping\n"
+f.write(headers)
+
+for container in containers:
+    brand = container.div.div.a.img['title']
+    
+    title_container = container.findAll("a",{"class":"item-title"})
+    product_name = title_container[0].text
+    
+    shipping_container = container.findAll("li",{"class":"price-ship"})
+    shipping = shipping_container[0].text.strip()
+    
+    
+    print("Brand: "+brand)
+    print("Product Name: "+product_name)
+    print("Shipping: "+shipping)
+    
+    f.write(brand + "," + product_name.replace(",","|") + "," + shipping + "\n")
+    
+    
+f.close()
+    
+
